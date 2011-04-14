0d0217afb43f089ce1452032db5da5ef6eed6f31
captproton <carl@captproton.com>
2011-04-13 21:10:42 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wdwdata::Application.config.secret_token = '16b607ae4f3e24bad5fca522bbd8d9bdb755b0701b8e1c145e5931b919d44c9ba73ef0e4371e7ffe449e5f2a61942a4d7b68493630ec5b9482419d3591b7c475'
